037a9c9efe34f6ceb4242a0d69f8ccbc577b3f8d
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2020-01-06 16:18:42 -0500

    Added a static floor

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Dimensions, StatusBar } from 'react-native';
 import Matter from "matter-js";
 import { GameEngine } from "react-native-game-engine";
+import Box from './components/Box.js'
 
 export default function App() {
-  return (
-    <GameEngine style={styles.container}>
-      <StatusBar hidden={true} />
-    </GameEngine>
-  );
-}
 
+  const { width, height } = Dimensions.get("screen");
+
+  const boxSize = Math.trunc(Math.max(width, height) * 0.075);
+
+  const initialBox = Matter.Bodies.rectangle(width / 2, height / 2, boxSize, boxSize);
+
+  const floor = Matter.Bodies.rectangle(width / 2, height - boxSize / 2, width, boxSize, { isStatic: true });
+
+  return (
+    <GameEngine
+      style={styles.container}
+      entities={{
+        initialBox: {
+          body: initialBox,
+          size: [boxSize, boxSize],
+          color: 'red',
+          renderer: Box
+        },
+        floor: {
+          body: floor,
+          size: [width, boxSize],
+          color: 'green',
+          renderer: Box
+        },
+      }} >
+        <StatusBar hidden={true} />
+      </GameEngine>
+      );
+    }
+    
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+        container: {
+        flex: 1,
+      backgroundColor: '#fff',
+    },
+  });
